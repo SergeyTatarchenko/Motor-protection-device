b9a155e4c46f05cd466e85dca1a7a6b095dddc3f
SergeyTatarchenko <zlojdigger@gmail.com>
2018-11-13 16:24:38 +0300

    - nothing to do
    Signed-off-by: SergeyTatarchenko <zlojdigger@gmail.com>

--- Code/system/include/stm32f0xx_tim.h
+++ Code/system/include/stm32f0xx_tim.h
@@ -13,6 +13,9 @@
 #define TIMER_7_STOP    (TIM7->CR1 &= ~TIM_CR1_CEN)
 #define TIMER_14_STOP   (TIM14->CR1&= ~TIM_CR1_CEN)
 
+#define CLEAR_BUFFER_TIM6   (TIM6->CNT =  0)
+#define CLEAR_BUFFER_TIM7   (TIM7->CNT =  0)
+#define CLEAR_BUFFER_TIM14  (TIM14->CNT = 0)
 
 /*-----------global function prototypes---------*/
 extern void GeneralTimerConfig(void);
